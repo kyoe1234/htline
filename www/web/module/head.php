e9d78669802606f3730bd6ea994c2f3d9a109fcc
kyoe1234@gmail.com <kyoe1234@gmail.com>
2013-01-12 15:55:04 +0900

    캐릭터셋 코드 수정

--- www/web/module/head.php
+++ www/web/module/head.php
@@ -1,4 +1,4 @@
-<meta http-equiv="Content-Type" content="text/html; charset=urf-8" />
+<meta charset="utf-8">
 <link rel="stylesheet" href="<?=URL_WEB_STATIC?>/bootstrap/css/bootstrap.css?ver=##__VERSION__##" />
 <!-- <link rel="shortcut icon" href="<?=URL_WEB_STATIC?>/image/favicon_gray.ico" /> -->
 <script src="<?=URL_WEB_STATIC?>/js/jquery-1.8.3.min.js"></script>
