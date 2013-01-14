b549c1ed8419ce292deb1555d8cf38e495c1a071
kyoe1234@gmail.com <kyoe1234@gmail.com>
2013-01-14 22:37:01 +0900

    메타태그 키워드 추가

--- www/web/module/head.php
+++ www/web/module/head.php
@@ -1,5 +1,5 @@
 <meta charset="utf-8">
-<meta name="keywords" content="게시판,익명게시판,htline,익며타임라인,hidden timeline" />
+<meta name="keywords" content="게시판,익명게시판,htline,익명타임라인,hidden timeline,대나무숲,비밀이야기" />
 <meta name="description" content="자유롭게 글을 남길수 있는 공간입니다." />
 <link rel="stylesheet" href="<?=URL_WEB_STATIC?>/bootstrap/css/bootstrap.css?ver=##__VERSION__##" />
 <!-- <link rel="shortcut icon" href="<?=URL_WEB_STATIC?>/image/favicon_gray.ico" /> -->
