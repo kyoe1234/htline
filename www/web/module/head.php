72667b7226f41ad4cc755220335402c0c56a02f8
lee kiho <kyoe1234@gmail.com>
2013-01-24 13:09:55 +0900

    google analytics install

--- www/web/module/head.php
+++ www/web/module/head.php
@@ -4,4 +4,18 @@
 <link rel="stylesheet" href="<?=URL_WEB_STATIC?>/bootstrap/css/bootstrap.css?ver=##__VERSION__##" />
 <!-- <link rel="shortcut icon" href="<?=URL_WEB_STATIC?>/image/favicon_gray.ico" /> -->
 <script src="<?=URL_WEB_STATIC?>/js/jquery-1.8.3.min.js"></script>
+
+<script type="text/javascript">
+	var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-37919834-1']);
+	_gaq.push(['_setDomainName', 'hoitalk.com']);
+	_gaq.push(['_trackPageview']);
+
+	(function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	})();
+</script>
+
 <title>HoiTalk: 자유롭게 글을 남기세요.</title>
